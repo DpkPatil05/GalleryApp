bc4745daabfa1365fdd3a56ca1e1838f977e49b9
Deepak Patil <patildeepak188@gmail.com>
2021-07-01 22:58:39 +0530

    added ui

---
+++ lib/common/color_palette.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class Palette {
+  Palette._();
+
+  // Basic colors
+  static const Color primary = Color(0xff9c27b0);
+  static const Color secondary = Color(0xffff1744);
+  static const Color tertiary = Color(0xffaf52bf);
+
+  static const Color fontColor = Color(0xff0d0d0d);
+  static const Color bg = Color(0xfff0fcff);
+}
---
+++ lib/screens/gallery_view.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gallery_app/common/color_palette.dart';
+import 'package:gallery_app/providers/drawer_provider.dart';
+import 'package:provider/provider.dart';
+
+class GalleryView extends StatefulWidget {
+  const GalleryView({Key? key}) : super(key: key);
+
+  @override
+  _GalleryViewState createState() => _GalleryViewState();
+}
+
+class _GalleryViewState extends State<GalleryView> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<DrawerProvider>(builder: (context, prov, _) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Gallery App',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Palette.primary,
+          shadowColor: Palette.tertiary,
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              MaterialButton(
+                child: Text('Settings'),
+                  onPressed: () {}
+                  ),
+              MaterialButton(
+                  child: Text('Gallery Page'),
+                  onPressed: () {}
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+}
